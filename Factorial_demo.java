4ede6984452637c6add27051b5e12512837176a3
Padmasri R <rpadmasri211@gmail.com>
2025-05-20 23:08:00 +0530

    Add files via upload

---
+++ Factorial_demo.java
@@ -0,0 +1,14 @@
+public class Factorial_demo {
+	public static void main(String[] args) {
+		{
+			int fact=1;
+			int i=1;
+			for(i=1;i<=10;i++) {
+				fact*=i;
+				System.out.println(i + " factorial is:"+fact);
+			}
+		}
+	}
+	
+
+}
